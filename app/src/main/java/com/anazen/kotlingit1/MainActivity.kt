51958eebbc3b1f88f5c5548d7c0deb1cb2b5c5f1
Nastya <Nastya@mail.ru>
2021-06-18 20:19:41 +0300

    my commit

--- app/src/main/java/com/anazen/kotlingit1/MainActivity.kt
+++ app/src/main/java/com/anazen/kotlingit1/MainActivity.kt
@@ -4,7 +4,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
-    //Это моц первый комментарий
+    //Это мой первый комментарий
+    // Это новый комментарий
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
